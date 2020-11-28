d7de9a8ff0beaf596344f5386ce5b9c6dd0afdfd
zhuny <zhuny@kaist.ac.kr>
2020-11-29 01:58:46 +0900

    show problem list

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import argparse
 import importlib
+from pathlib import Path
+
+
+def show_list():
+    print("List of problems are the following")
+
+    for problem_path in Path('problem').glob("*"):
+        print(problem_path.name)
+        problem = importlib.import_module(f"problem.{problem_path.name}")
+        print("   ", problem.__doc__.strip())
 
 
 def main():
@@ -10,7 +20,7 @@ def main():
     args = parser.parse_args()
 
     if args.l:
-        print("List of problems are the following")
+        show_list()
 
     elif args.problem is None:
         print("Problem should be given")
@@ -22,6 +32,7 @@ def main():
             return
         except ImportError:
             print(f"Problem {args.problem!r} is not found")
+            show_list()
 
     print()
     parser.print_help()
